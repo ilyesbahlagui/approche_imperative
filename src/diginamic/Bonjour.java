3f19f87c207a56c460354ae31b8625abc15f7b37
Ilyes Bahlagui <ilyesbahlagui@gmail.com>
2022-10-06 15:22:13 +0200

    Add news files

---
+++ src/diginamic/Bonjour.java
@@ -0,0 +1,18 @@
+package diginamic;
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bonjour tout le monde  !");
+		int a =1;
+		String maChaineString="Bonjour,";
+		char ch='a';
+		double toto=152.36d;
+		String b="bonjour \u00a9";
+		System.out.println(b);
+		
+		
+	}
+
+}
